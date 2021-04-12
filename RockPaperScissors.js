511dd0f5751b210947fa4b600343f57258e4bc23
daysh001 <64609311+daysh001@users.noreply.github.com>
2021-04-12 08:36:54 +0100

    Main functionality done

--- RockPaperScissors.js
+++ RockPaperScissors.js
@@ -1,120 +1,99 @@
-// //globals
+//globals
 let playerScore = 0;
 let computerScore = 0;
-let toggleCompTurn = false;
+let tieScore = 0;
 
-function start() {
-  toggleCompTurn = false;
-  let winner = document.getElementById("whoWon");
-  winner.style.display = "none";
-}
-
-//the players choice
-//the player icon changes to its choice depending which button has been clicked
-function playerPlay() {
-  let btnRock = document.querySelector("#playerRock");
-  btnRock.addEventListener("click", () => {
-    document.getElementById("playerChoiceImg").src = "images/Rock.png";
-    return 1;
-  });
-
-  let btnPaper = document.querySelector("#playerPaper");
-  btnPaper.addEventListener("click", () => {
-    document.getElementById("playerChoiceImg").src = "images/Paper.png";
-    return 2;
-  });
-
-  let btnScissors = document.querySelector("#playerScissors");
-  btnScissors.addEventListener("click", () => {
-    document.getElementById("playerChoiceImg").src = "images/Scissors.png";
-    return 3;
-  });
-
-  toggleCompTurn = true;
-}
-
-//the computers choice
+//the computers choice 
 function computerPlay() {
-  if (toggleCompTurn === true) {
-    let computerChoice = Math.floor(Math.random() * 3 + 1);
+  let computerChoice = Math.floor((Math.random() * 3) + 1);
 
-    switch (
-    computerChoice //changes the computers icon to its choice
-    ) {
-      case 1:
-        document.getElementById("computerChoiceImg").src = "images/Rock.png";
-        return 1;
-        break;
+  switch (computerChoice) {
+    case 1:
+      console.log("Computer: Rock");
+      document.getElementById("computerChoiceImg").src = "images/Rock.png";
+      return 1;
+      break;
 
-      case 2:
-        document.getElementById("computerChoiceImg").src = "images/Paper.png";
-        return 2;
-        break;
+    case 2:
+      console.log("Computer: Paper");
+      document.getElementById("computerChoiceImg").src = "images/Paper.png";
 
-      case 3:
-        document.getElementById("computerChoiceImg").src = "images/Scissors.png";
-        return 3;
-        break;
+      return 2;
+      break;
 
-      default:
-        alert.log("Something has gone wrong.");
-    }
-    toggleCompTurn = false;
+    case 3:
+      console.log("Computer: Scissors");
+      document.getElementById("computerChoiceImg").src = "images/Scissors.png";
+      return 3;
+      break;
+
+    default:
+      console.log("Something has gone wrong.");
   }
+}
 
-  
+//the players choice based on which button is pressed
+function playerPlay() {
+
+  let playerRock = document.getElementById("playerRock");
+  let playerPaper = document.getElementById("playerPaper");
+  let playerScissors = document.getElementById("playerScissors");
+
+  playerRock.addEventListener("click", () => {
+    document.getElementById("playerChoiceImg").src = "images/Rock.png";
+    playRound(1, computerPlay());
+  })
+
+  playerPaper.addEventListener("click", () => {
+    document.getElementById("playerChoiceImg").src = "images/Paper.png";
+    playRound(2, computerPlay());
+  })
+
+  playerScissors.addEventListener("click", () => {
+    document.getElementById("playerChoiceImg").src = "images/Scissors.png";
+    playRound(3, computerPlay());
+  })
 
 }
 
-//win and lose logic per round
+//logic to play a round and score
 function playRound(playerSelection, computerSelection) {
-  if (
-    ((playerSelection == 2 || 3) && playerSelection > computerSelection) ||
-    (playerSelection === 1 && computerSelection === 3)
-  ) {
-    playerScore++; 
-
+  if (((playerSelection == 2 || 3) && (computerSelection == (playerSelection - 1))) || ((playerSelection === 1) && computerSelection === 3)) {
+    playerScore++;
     let playerWins = document.querySelector("#playerWins");
     playerWins.textContent = playerScore;
   } else if (playerSelection === computerSelection) {
-
-    
+    tieScore++;
+    let ties = document.querySelector("#ties");
+    ties.textContent = tieScore;
   } else {
-    computerScore++;  // append computerScore to computerWins ID
-
+    computerScore++;
     let computerWins = document.querySelector("#computerWins");
     computerWins.textContent = computerScore;
   }
-}
 
-// Winner and loser logic overall (game over)
-function gameOver() {
-  //displays overall score and winner
-  if (playerScore > computerScore) {  // whoWon == "Player Won"  
-    console.log(
-      `The game is over, the player has won!. The final score is p:${playerScore} and C:${computerScore}.`
-    );
-  } else if (computerScore > playerScore) {
-    console.log(  // whoWon == "Computer Won"
-      `The game is over, the computer has won! The final score is p:${playerScore} and C:${computerScore}.`
-    );
-  } else {
-    console.log(
-      `The game is over, it's a draw! The final score is p:${playerScore} and C:${computerScore}.`
-    );
-  }
-}
-
-//runs the game
-function game() {
-
- // let playerChoices = document.getElementById("playerChoices");
-  playRound(playerPlay(), computerPlay());
   gameOver();
 }
 
-// **** GAME ****
-start();
+//winner logic (overall gameover)
+function gameOver() {
+  let winner = document.querySelector("#winner");
+  let buttons = document.querySelectorAll("button");
 
-let playButton = document.getElementById("playBtn");
-playButton.addEventListener('click', game); //starts the game 
+  if (playerScore === 5){
+    winner.textContent = "Player has Won!";
+    winner.style = "display: inline"
+  } else if(computerScore === 5){
+    winner.textContent = "Computer has won!";
+    winner.style = "display: inline";
+  }
+
+  if (playerScore === 5 || computerScore === 5){
+  buttons.forEach(button => {
+    button.style = "display: None"
+  });
+}
+
+}
+
+playerPlay();
